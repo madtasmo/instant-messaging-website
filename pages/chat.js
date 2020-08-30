41aa0052f2c0c07d2e42bbb8fa77ef4ac984e68d
Adam <30482667+madtasmo@users.noreply.github.com>
2020-08-30 23:54:17 +0100

    Update chat.js

--- pages/chat.js
+++ pages/chat.js
@@ -11,42 +11,70 @@ export default function Home() {
 
             <main>
 
-                <h1 className={styles.title}></h1>
+                
                 
                 <div className={styles.chatcontainer}>
-
+                    {/** 
+                     * the box outline of the chat window
+                    */}
                 <div className={styles.messagescontainer}>
+                        {/**
+                         * The top window of the chat window
+                         */}
                     <div id="ingoingMessage">
-                        <p className={styles.user}>Login</p>
+                            {/**
+                             * Example of an ingoing message
+                             */}
+                            <p className={styles.user}>Login</p>{/**Username*/}
+                            
                         <div className={styles.ingoingmessage}>
                             <img className={styles.avatar} src="https://cdn.discordapp.com/attachments/696332325457231897/749733699595141170/32708c6077b3c9341b825d619351d417.png">
 
-                            </img>
+                                </img>{/**Avatr*/}
                             <div className={styles.tablebreak}>
-                                <p className={styles.message}>Hello there anakin!</p>
+                                    <p className={styles.imessage}>Hello there anakin!</p> {/**Example message*/}
                             </div>
                         </div>
-                        <p className={styles.timestamp}>00:00 am</p>   
-                    </div>
-                        <div id="ingoingMessage">
-                            <p className={styles.user}>Login</p>
-                            <div className={styles.ingoingmessage}>
-                                <img className={styles.avatar} src="https://cdn.discordapp.com/attachments/696332325457231897/749733699595141170/32708c6077b3c9341b825d619351d417.png">
+                            <p className={styles.timestamp}>00:00 am</p>  {/**Timestamp*/} 
+                </div>
+
+
+                        <div id="outgoingmessage">
+                            {/**
+                             * This is for outgoing messages sent by the user
+                             * Here, the timestamps and avatar are rotated to ensure the best possible symetry
+                             */}
+                            <div className={styles.outgoingmessage}>
+                                
+                                
+                                <p className={styles.timestamp2}>00:00 am</p>
+                                <br></br>{/**dont ask why pls */}
+                                <div className={styles.tablebreak2}>
+
+                                    <p className={styles.omessage}>Example dasa ssa ddsa d a das asd aas asa s </p>{/**Example of ingoing message*/}
+
+                                </div>
+                                <img className={styles.avatar2} src="https://cdn.discordapp.com/attachments/696332325457231897/749733699595141170/32708c6077b3c9341b825d619351d417.png">
 
                                 </img>
-                                <div className={styles.tablebreak}>
-                                    <p className={styles.message}>Hello there anakin!asd dd dd d dd  d d dd d dd d d dddd dd d d ddd</p>
-                                </div>
+                                <p className={styles.user2}>Login</p>
+                                
+                                
                             </div>
-                            <p className={styles.timestamp}>00:00 am</p>
+                           
                         </div>
+                        
+                        
                 </div>
 
 
 
-
+                    {/**
+                     * Bottom box of the chat window
+                     * Used for sending text
+                    */}
                 <div className={styles.textmessagecontainer}>
-                    <input className={styles.textbox}></input>
+                        <input className={styles.textbox}></input>{/**Actual input field*/}
                 </div>
                 </div>
                 
